d2ab9ab45f0f09c58dc4b51fa7389a8e2daee723
BUKOLA OMOTOSO <bukola_omotoso@BUKOLAs-MacBook-Pro.local>
2017-07-09 14:53:13 -0500

    creates a new route to display info about a campground

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ app.set("view engine","ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
-// 	name: "Campground B",
-// 	image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"
+// 	name: "Family Camp",
+// 	image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg",
+// 	description:"This is a family campground, spacious enough for all family members"
 // }, function(err, campground)	{
 // 	if (err)
 // 		console.log(err);
@@ -38,7 +40,7 @@ app.get("/campgrounds", function(req, res)	{
 		if (err)	{
 			console.log(err);
 		}	else	{
-			res.render("campgrounds", {campgrounds:allcampgrounds});
+			res.render("index", {campgrounds:allcampgrounds});
 		}
 	})
 })
@@ -47,7 +49,8 @@ app.get("/campgrounds", function(req, res)	{
 app.post("/campgrounds", function(req, res)	{
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name:name, image: image};
+	var desc = req.body.description;
+	var newCampground = {name:name, image: image, description:desc};
 	
 	Campground.create(newCampground, function(err, newlyCreated){
 			if(err)	{
@@ -60,6 +63,16 @@ app.post("/campgrounds", function(req, res)	{
 
 app.get("/campgrounds/new", function(req, res)	{
 	res.render("new");
+})
+
+app.get("/campgrounds/:id", function(req, res)	{
+	Campground.findById(req.params.id, function(err, foundCampground)	{
+		if(err)
+			console.log(err);
+		else	{
+				res.render("show", {campground:foundCampground});
+		}
+	})
 })
 
 	
