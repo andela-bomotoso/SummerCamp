8b0982a91f5765e6624865fd3feed8c6b403c0db
andela-bomotoso <gracomot@gmail.com>
2017-07-17 18:27:13 -0500

    adds comments to campgrounds

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
-var express = require("express");
-	bodyParser = require('body-parser')
-	app = express();
-	mongoose =  require("mongoose")
-
+var express = require("express"),
+	bodyParser = require('body-parser'),
+	app = express(),
+	mongoose =  require("mongoose"),
+	Campground = require("./models/campground");
+	Comment = require("./models/comment");
+	seedDB = require("./seeds");
 	mongoose.connect("mongodb://localhost/summercamp");
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine","ejs");
-
-
-// SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create({
-// 	name: "Family Camp",
-// 	image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg",
-// 	description:"This is a family campground, spacious enough for all family members"
-// }, function(err, campground)	{
-// 	if (err)
-// 		console.log(err);
-// 	else
-// 		console.log(campground);
-// })
-
+	app.use(bodyParser.urlencoded({extended: true}));	
+	app.set("view engine","ejs");
+	app.use(express.static(__dirname+"/public"))
+	seedDB();
 
 
 app.get("/", function(req, res){
@@ -40,11 +21,10 @@ app.get("/campgrounds", function(req, res)	{
 		if (err)	{
 			console.log(err);
 		}	else	{
-			res.render("index", {campgrounds:allcampgrounds});
+			res.render("campgrounds/index", {campgrounds:allcampgrounds});
 		}
 	})
 })
-
 
 app.post("/campgrounds", function(req, res)	{
 	var name = req.body.name;
@@ -62,21 +42,50 @@ app.post("/campgrounds", function(req, res)	{
 })
 
 app.get("/campgrounds/new", function(req, res)	{
-	res.render("new");
+	res.render("campgrounds/new");
 })
 
 app.get("/campgrounds/:id", function(req, res)	{
-	Campground.findById(req.params.id, function(err, foundCampground)	{
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground)	{
 		if(err)
 			console.log(err);
-		else	{
-				res.render("show", {campground:foundCampground});
+		else	{	
+				console.log(foundCampground);
+				res.render("campgrounds/show", {campground:foundCampground});
 		}
 	})
 })
 
-	
+app.get("/campgrounds/:id/comments/new", function(req, res)	{
+	Campground.findById(req.params.id, function(err, campground)	{
+		if (err)	{
+			console.log(err);
+		}	else	{
+			res.render("comments/new", {campground:campground});
+		}
+	});
 
-app.listen(3000, function()	{
+});
+
+app.post("/campgrounds/:id/comments", function(req, res)	{
+	Campground.findById(req.params.id, function(err, campground)	{
+		if(err)	{
+			res.redirect("/campgrounds");
+		}	else	{
+			Comment.create(req.body.comment, function(err, comment)	{
+				if(err)	{
+					console.log(err);
+				}	else	{
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect('/campgrounds/'+ campground._id);
+				}
+			});
+		}
+	});
+
+});
+
+app.listen(3003, function()	{
 	console.log("The summer camp server is up");
 })
---
+++ seeds.js
@@ -0,0 +1,60 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data  = [
+{
+	name: "Cloud's Rest",
+	image: "https://farm4.staticflickr.com/3282/2770447094_2c64348643.jpg",
+	description: "Very nice campground facility"
+},
+	{
+	name: "Mini House",
+	image: "https://farm5.staticflickr.com/4101/4961777592_322fea6826.jpg",
+	description: "A campground with a touch of home feeling"
+},
+
+{
+	name: "Shady Campground",
+	image: "https://farm7.staticflickr.com/6089/6094103869_d53a990c83.jpg",
+	description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc"
+}
+
+]
+
+function seedDB()	{
+	Campground.remove({}, function(err)	{
+	if (err)	
+		console.log(err);
+	else	{
+		console.log("All campgrounds removed");
+	}
+});
+	data.forEach(function(seed)	{
+	Campground.create(seed, function(err, campground)	{
+		if (err)	{
+			console.log(err);
+		}	else	{
+			console.log("added a campground");
+			Comment.create(
+			{
+				text: "This is a great place but I wish there was internet",
+				author: "Homer"
+			}, function(err, comment)	{
+				if (err)	{
+					console.log(err);
+				}	else	{
+					campground.comments.push(comment);
+					campground.save();
+					console.log("Created new comment");
+				}
+			})
+		}
+	});
+});
+}
+
+
+
+module.exports = seedDB;
+
