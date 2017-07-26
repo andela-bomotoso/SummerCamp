4bca56f31fac80137ea1e6d30481fea961684c71
andela-bomotoso <bukola.omotoso@andela.com>
2017-07-25 20:59:19 -0500

    adds mongolab for managing the mongo db

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var express = require("express"),
 		indexRoutes = require("./routes/index")
 
 
-	mongoose.connect("mongodb://localhost/summercamp");
+	//mongoose.connect("mongodb://localhost/summercamp");
+	mongoose.connect("mongodb://grace:password@ds125053.mlab.com:25053/summercamp");
 
 	app.use(bodyParser.urlencoded({extended: true}));	
 	app.set("view engine","ejs");
